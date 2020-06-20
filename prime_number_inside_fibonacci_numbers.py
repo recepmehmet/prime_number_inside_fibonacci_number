d8189c75242ce176b5e9707ef01ca66eb8d1f6e8
Tarık İNCE <incetarik@hotmail.com>
2020-06-20 22:35:08 +0300

    Improvements

--- prime_number_inside_fibonacci_numbers.py
+++ prime_number_inside_fibonacci_numbers.py
@@ -1,44 +1,27 @@
-def Fibonacci(desired_times_term):
-    number_one = 0
+def fibonacci(amount):
+    number_one = 1
     number_two = 1
-    counter_of_fibonacci_terms = 0
-    fibonacci_numbers = []
 
-    if desired_times_term <= 0:
-        print("Please, Enter a positive number")
-    elif desired_times_term == 1:
-        print("Fibonacci value : ", number_one)
-    else:
-        while counter_of_fibonacci_terms < desired_times_term:
-            print(number_one)
-            fibonacci_numbers.append(number_one)
-            number_th = number_one + number_two
-            number_one = number_two
-            number_two = number_th
-            counter_of_fibonacci_terms += 1
+    if amount <= 0:
+        print("Please enter a positive number")
+        return
 
-    return fibonacci_numbers
+    if amount == 1:
+        print("Fibonacci value:", number_one)
+        return
 
-def is_prime_number(a_number):
-    if a_number > 1:
-        # check for factors
-        for i in range(2, a_number):
-            if (a_number % i) == 0:
-                break
-        else:
-            return a_number
+    for _ in range(0, amount):
+        yield number_one
+        number_one, number_two = number_two, number_one + number_two
 
+def is_prime(number):
+    return next((False for i in range(2, number) if number % i == 0), number)
 
-def prime_number_finder_inside_a_array(a_array):
-    prime_number_keeper = []
-    for array_counter in a_array:
-        if is_prime_number(int(array_counter)) != None:
-            prime_number_keeper.append(is_prime_number(int(array_counter)))
+def collect_primes_from(source):
+    return [ item for item in source if is_prime(item) and item > 1 ]
 
-    return prime_number_keeper
-
-
-taken_number_of_fibo = input("Please enter number time of fibonacci series")
-fibonacci_numbers = Fibonacci(int(taken_number_of_fibo))
-prime_numbers_in_fibonacci_desired_times_number = prime_number_finder_inside_a_array(fibonacci_numbers)
-print(prime_numbers_in_fibonacci_desired_times_number)
+amount = input("Please enter number time of fibonacci series: ")
+amount = int(amount)
+fibonacci_generator = fibonacci(amount)
+result = collect_primes_from(fibonacci_generator)
+print(result)
